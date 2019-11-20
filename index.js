ec44ca49e6c60e39ab9af4a0cabd092be43b7885
Herr Mohsen Dana | COMP.net GmbH <mdana@compnetgmbh.de>
2019-11-20 12:14:08 +0100

    first commit new

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+.then(() => console.log('Connected to MongoDB...'))
+.catch( err => console.error('Could not Connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
